5bed9c21a5850bab1a3929c10ac394a035bc3cf8
coderBoi963 <cheezepie963@gmail.com>
2022-10-12 00:05:41 +0530

    Added new music features. Also added DM to creator system

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ const distube = new DisTube(client, {
     emitAddSongWhenCreatingQueue: false,
     emitAddListWhenCreatingQueue: false,
     nsfw: true,
-})
+});
 
 const PREFIX = "-"; //Sets the Command Prefix
 const AnimeSubreddits = ['AnimeFunny', 'Animememes'];
@@ -50,21 +50,25 @@ client.on('ready', () => {
 
 //This sends a DM to me informing which discord server added Cowie
 client.on('guildCreate', guild => {
-    client.users.get('812753087545737217').send(guild.name + " added me! YAY!");
+        client.users.send("812753087545737217", guild.name+" added me! Yay!");
+});
+//This sends a DM to me informing which discord server kicked Cowie
+client.on('guildDelete', guild => {
+        client.users.send("812753087545737217", guild.name+" kicked me out :(");
 });
 
 client.on('messageCreate', async (message) => {
-    const msg = message.content.toLowerCase().trim();
+    const msg = message.content.trim();
     if (msg.startsWith(PREFIX)) {
         const command = msg.substring(PREFIX.length).split(" ");
-        if (command[0] === 'waifu') {
+        if (command[0].toLowerCase() === 'waifu') {
             fetch('https://api.waifu.pics/sfw/waifu')
                 .then(res => res.json())
                 .then(data => {
                     message.channel.send(data.url);
                 })
         }
-        if (command[0] === 'waifu-nsfw') {
+        if (command[0].toLowerCase() === 'waifu-nsfw') {
             if (message.channel.nsfw) {
                 fetch('https://api.waifu.pics/nsfw/waifu')
                     .then(res => res.json())
@@ -75,7 +79,7 @@ client.on('messageCreate', async (message) => {
             else
                 message.channel.send(`You must be in an NSFW channel to view this content:exclamation:`);
         }
-        if (command[0] === 'neko-nsfw') {
+        if (command[0].toLowerCase() === 'neko-nsfw') {
             if (message.channel.nsfw) {
                 fetch('https://api.waifu.pics/nsfw/neko')
                     .then(res => res.json())
@@ -86,14 +90,14 @@ client.on('messageCreate', async (message) => {
             else
                 message.channel.send(`You must be in an NSFW channel to view this content:exclamation:`);
         }
-        else if (command[0] === 'neko') {
+        else if (command[0].toLowerCase() === 'neko') {
             fetch('https://api.waifu.pics/sfw/neko')
                 .then(res => res.json())
                 .then(data => {
                     message.channel.send(data.url);
                 })
         }
-        else if (command[0] === 'quote') {
+        else if (command[0].toLowerCase() === 'quote') {
             fetch('https://animechan.vercel.app/api/random')
                 .then(response => response.json())
                 .then((quote) => {
@@ -104,7 +108,7 @@ client.on('messageCreate', async (message) => {
                 }
                 )
         }
-        else if (command[0] === 'meme') {
+        else if (command[0].toLowerCase() === 'meme') {
             const randomSubreddit = AnimeSubreddits[Math.floor(Math.random() * AnimeSubreddits.length)];
             redditFetch({
                 subreddit: randomSubreddit,
@@ -119,7 +123,7 @@ client.on('messageCreate', async (message) => {
                 message.channel.send({ embeds: [embed] });
             });
         }
-        else if (command[0] === 'cute') {
+        else if (command[0].toLowerCase() === 'cute') {
             const randomCutePost = cuteAnimePosts[Math.floor(Math.random() * cuteAnimePosts.length)];
             fetch(`https://api.waifu.pics/sfw/${randomCutePost}`)
                 .then(res => res.json())
@@ -128,22 +132,25 @@ client.on('messageCreate', async (message) => {
                     message.channel.send({ embeds: [embed] });
                 })
         }
-        else if (command[0] === 'play' && command.length > 1) {
+        else if (command[0].toLowerCase() === 'play' && command.length > 1) {
             if (!message.member.voice.channel)
                 message.reply("You must be connected to a voice channel to play music");
             else {
                 message.reply("Searching for the song...");
-                let RequestedSong = '';
-                for (let i = 1; i < command.length; i++)
-                    RequestedSong = RequestedSong + command[i];
+                command.shift();
+                const RequestedSong = command.join(" ");
                 distube.play(message.member.voice.channel, RequestedSong, {
                     member: message.member,
                     textChannel: message.channel,
                     message
+                })
+                .catch(err=>{
+                    if(err)
+                        message.reply(":exclamation:Could not find the video url or something went wrong:exclamation:");
                 });
             }
         }
-        else if (command[0] == 'play') {
+        else if (command[0].toLowerCase() == 'play') {
             if (!message.member.voice.channel)
                 message.reply("You must be connected to a voice channel to play music");
             else {
@@ -156,7 +163,7 @@ client.on('messageCreate', async (message) => {
                 });
             }
         }
-        else if (command[0] === 'stop') {
+        else if (command[0].toLowerCase() === 'stop') {
             if (!message.member.voice.channel)
                 message.reply("You cannot stop a song if you are not in a voice channel");
             else if (!distube.getQueue(message))
@@ -166,7 +173,7 @@ client.on('messageCreate', async (message) => {
                 distube.stop(message);
             }
         }
-        else if (command[0] === 'skip') {
+        else if (command[0].toLowerCase() === 'skip') {
             if (!message.member.voice.channel)
                 message.reply("You cannot skip a song if you are not in a voice channel");
             else if (!distube.getQueue(message))
@@ -200,7 +207,7 @@ client.on('messageCreate', async (message) => {
                 }
             }
         }
-        if (command[0] === "pause") {
+        if (command[0].toLowerCase() === "pause") {
             if (!message.member.voice.channel)
                 message.reply("You cannot pause a song if you are not in a voice channel");
             else if (!distube.getQueue(message))
@@ -212,7 +219,7 @@ client.on('messageCreate', async (message) => {
                 distube.pause(message);
             }
         }
-        if (command[0] === "resume") {
+        if (command[0].toLowerCase() === "resume") {
             if (!message.member.voice.channel)
                 message.reply("You cannot resume a song if you are not in a voice channel");
             else if (!distube.getQueue(message))
@@ -224,8 +231,40 @@ client.on('messageCreate', async (message) => {
                 distube.resume(message);
             }
         }
+        if (command[0].toLowerCase() === "repeat") {
+            if (!message.member.voice.channel)
+                message.reply("You cannot repeat a song if you are not in a voice channel");
+            else if (!distube.getQueue(message))
+                message.reply("Nothing to repeat, the queue is empty");
+            else if(command.length==1)
+                message.reply(`You must specify a repeat type...\n0-> No repeat\n1-> Repeat the current song\n2-> Repeat the queue\n`);
+            else {
+                if (parseInt(command[1])==0)
+                    message.reply("Current Song/Queue will not repeat");
+                else if(parseInt(command[1])==1)
+                    message.reply("Current Song will now repeat");
+                else if(parseInt(command[1])==2)
+                    message.reply("Current Queue will now repeat");
+                distube.setRepeatMode(message, parseInt(command[1]));
+            }
+        }
+        if (command[0].toLowerCase() === "queue") {
+            const queue = distube.getQueue(message);
+            if (!queue)
+                message.reply("Queue is empty");
+            else {
+                const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+                const embed = new EmbedBuilder()
+                    .setColor(randomColor)
+                    .setTitle("Current queue:")
+                    .setDescription(queue.songs.map((song, id) =>
+                        `**${id + 1}**. [${song.name}] - \`${song.formattedDuration}\``
+                    ).join("\n"))
+                message.channel.send({ embeds: [embed] });
+            }
+        }
 
-        else if (command[0] === 'help') {
+        else if (command[0].toLowerCase() === 'help') {
             const embed = new EmbedBuilder()
                 .setTitle('Hello Senpai! I am Cowie and I am here to assist you')
                 .addFields(
@@ -235,11 +274,12 @@ client.on('messageCreate', async (message) => {
                     { name: PREFIX + 'meme', value: 'Gives a random anime meme' },
                     { name: PREFIX + 'cute', value: 'Cute wholesome anime posts' },
                     { name: PREFIX + 'play <Song_Name>', value: 'Plays the specified anime song. If only no <Song_Name> is provided then I\'ll play a random anime song' },
-                    { name: PREFIX + 'skip', value: 'Skips the song' },
+                    { name: PREFIX + 'skip', value: 'Skips the song. Skips <Number> will skip to the <Number> song in the queue. skip -1 will play the previous song' },
                     { name: PREFIX + 'pause', value: 'Pauses the song' },
                     { name: PREFIX + 'resume', value: 'Resumes the paused song' },
-                    { name: PREFIX + 'stop', value: 'Stops the song and disconnects the bot from the voice channel' }
-                    // { name: PREFIX + 'voice', value: 'Plays a random anime kawaii voice' },
+                    { name: PREFIX + 'stop', value: 'Stops the song and disconnects the bot from the voice channel' },
+                    { name: PREFIX + 'repeat <Mode>', value: `if <Mode>=0, then the song/queue won't repeat. If <Mode>=1, then the current song will repeat. If <Mode>=2, then the entire playlist will repeat`},
+                    { name: PREFIX + 'queue', value: 'Shows the current queue'}
                     // { name: '||' + PREFIX + 'waifu-nsfw||', value: '||Posts waifu NSFW content. You must be in an NSFW channel to use this command. At your own risk||' },
                     // { name: '||' + PREFIX + 'neko-nsfw||', value: '||Posts neko NSFW content. You must be in an NSFW channel to use this command. At your own risk||' }
                 )
@@ -258,15 +298,15 @@ distube.on('playSong', (queue, song) => {
         .setTitle("Now Playing...")
         .setDescription(song.name)
         .setThumbnail(song.thumbnail)
-    queue.textChannel.send({ embeds: [embed] })
-})
+    queue.textChannel.send({ embeds: [embed] });
+});
 distube.on('addSong', (queue, song) => {
     const randomColor = Math.floor(Math.random() * 16777215).toString(16);
     const embed = new EmbedBuilder()
         .setColor(randomColor)
         .setTitle("New song added to the queue 👍")
     queue.textChannel.send({ embeds: [embed] });
-})
+});
 
 client.login(process.env.BOT_TOKEN);
 
